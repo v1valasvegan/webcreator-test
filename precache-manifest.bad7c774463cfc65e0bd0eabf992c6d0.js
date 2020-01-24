8a0197f36a21bb7a925a4a67b5dd34bbea000e67
Victor Zhuravlev <mocambe@mail.ru>
2020-01-24 09:22:42 +0300

    Updates

---
+++ precache-manifest.bad7c774463cfc65e0bd0eabf992c6d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96c66991318d3879e8a29098471f0d33",
+    "url": "/webcreator-test/index.html"
+  },
+  {
+    "revision": "e91aacccbaa694843c53",
+    "url": "/webcreator-test/static/css/main.5a494b91.chunk.css"
+  },
+  {
+    "revision": "a0d52411f18bcff093a5",
+    "url": "/webcreator-test/static/js/2.0fef9c62.chunk.js"
+  },
+  {
+    "revision": "30d8ef3813de0269a8460a5317dd0eba",
+    "url": "/webcreator-test/static/js/2.0fef9c62.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e91aacccbaa694843c53",
+    "url": "/webcreator-test/static/js/main.7053ec9d.chunk.js"
+  },
+  {
+    "revision": "6f05d7fe5a421dfc7c37",
+    "url": "/webcreator-test/static/js/runtime-main.efbd61de.js"
+  }
+]);
